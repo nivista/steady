77cc7d66730de8a6e63f666d44793157ed5b632e
nivista <syaniv@live.com>
2020-06-28 14:19:33 -0700

    update store/client

--- store/client.go
+++ store/client.go
@@ -12,8 +12,9 @@ import (
 	"github.com/nivista/steady/timer"
 )
 
+// Client represents a client to the data store.
 type Client struct {
-	*pgxpool.Pool
+	conn *pgxpool.Pool
 }
 
 type (
@@ -27,15 +28,21 @@ const (
 	interval scheduleType = "INTERVAL"
 )
 
+// NewClient returns a new client to the data store.
 func NewClient(ctx context.Context) (*Client, error) {
 	dbpool, err := pgxpool.Connect(context.Background(), os.Getenv("DATABASE_CONN"))
 	if err != nil {
 		return nil, err
 	}
-
-	return &Client{Pool: dbpool}, nil
+	return &Client{dbpool}, nil
 }
 
+// Close closes the database client.
+func (c *Client) Close() {
+	c.conn.Close()
+}
+
+// CreateTimer persists a timer to the data store.
 func (c *Client) CreateTimer(ctx context.Context, t timer.Timer) error {
 	var tt taskType
 	switch task := t.Task.(type) {
@@ -64,7 +71,7 @@ func (c *Client) CreateTimer(ctx context.Context, t timer.Timer) error {
 		return err
 	}
 
-	_, err = c.Query(ctx,
+	_, err = c.conn.Query(ctx,
 		`insert into timers (id, domain, executionCount, taskType, task, scheduleType, schedule, meta) 
 			values ($1, $2, $3, $4, $5, $6, $7, $8)
 			on conflict do nothing`,
@@ -73,13 +80,15 @@ func (c *Client) CreateTimer(ctx context.Context, t timer.Timer) error {
 	return err
 }
 
+// DeleteTimer deletes a timer from the data store.
 func (c *Client) DeleteTimer(ctx context.Context, domain string, id uuid.UUID) error {
-	_, err := c.Query(ctx, //TODO check if a timer was deleted
+	_, err := c.conn.Exec(ctx,
 		`delete from timers where domain=$1 and id=$2`,
 		domain, id)
 	return err
 }
 
+// GetTimer gets a timer from the data store.
 func (c *Client) GetTimer(ctx context.Context, domain string, id uuid.UUID) (*timer.Timer, error) {
 	t := timer.Timer{}
 
@@ -87,7 +96,7 @@ func (c *Client) GetTimer(ctx context.Context, domain string, id uuid.UUID) (*ti
 	var st scheduleType
 	var taskBytes, scheduleBytes []byte
 
-	err := c.QueryRow(ctx,
+	err := c.conn.QueryRow(ctx,
 		`select id, domain, executionCount, taskType, task, scheduleType, schedule, meta from timers 
 			where domain=$1 and id=$2`,
 		domain, id).Scan(&t.ID, &t.Domain, &t.ExecutionCount, &tt, &taskBytes, &st, &scheduleBytes, &t.Meta)
@@ -101,11 +110,11 @@ func (c *Client) GetTimer(ctx context.Context, domain string, id uuid.UUID) (*ti
 		var task timer.HTTP
 		err = json.Unmarshal(taskBytes, &task)
 		if err != nil {
-			return nil, err // TODO, make custom error types
+			return nil, err
 		}
 		t.Task = &task
 	default:
-		return nil, errors.New("Unknown type")
+		return nil, errors.New("Unknown task type")
 	}
 
 	switch st {
@@ -118,19 +127,20 @@ func (c *Client) GetTimer(ctx context.Context, domain string, id uuid.UUID) (*ti
 		t.Schedule = &schedule
 	case interval:
 		var schedule timer.Interval
-		err := json.Unmarshal(scheduleBytes, schedule)
+		err := json.Unmarshal(scheduleBytes, &schedule)
 		if err != nil {
 			return nil, err
 		}
 		t.Schedule = &schedule
 	default:
-		return nil, errors.New("Unknown type")
+		return nil, errors.New("Unknown schedule type")
 	}
 	return &t, nil
 }
 
+// SetExecCount sets the execution count of a timer.
 func (c *Client) SetExecCount(ctx context.Context, domain string, id uuid.UUID, count int) error {
-	_, err := c.Query(ctx,
+	_, err := c.conn.Exec(ctx,
 		`update timers
 			set executionCount=$1
 			where domain=$2 and id=$3`,
