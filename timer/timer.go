c09a6ed485004c38220b00231e6fef3a01595f06
nivista <syaniv@live.com>
2020-08-12 15:15:14 -0700

    add timer run

--- timer/timer.go
+++ timer/timer.go
@@ -1,7 +1,10 @@
 package timer
 
 import (
+	"sync/atomic"
 	"time"
+
+	"github.com/jonboulle/clockwork"
 )
 
 type (
@@ -28,3 +31,51 @@ type (
 		schedule(prog Progress, now time.Time) (nextFire time.Time, done bool)
 	}
 )
+
+func getCanceller() (stopCh chan struct{}, cancelFn func()) {
+	var stopped int32
+
+	stopCh = make(chan struct{})
+
+	cancelFn = func() {
+		if atomic.CompareAndSwapInt32(&stopped, 0, 1) {
+			close(stopCh)
+		}
+	}
+
+	return
+}
+
+// Run starts the timers execution.
+func (t *Timer) Run(updateProgress func(Progress), finishTimer func(), initialProgress Progress, clock clockwork.Clock) (cancel func()) {
+
+	var progress = initialProgress
+
+	stopCh, cancel := getCanceller()
+
+	go func() {
+		for {
+			nextFire, done := t.scheduler.schedule(progress, clock.Now())
+
+			if done {
+				finishTimer()
+				cancel()
+				return
+			}
+
+			select {
+			case <-clock.After(nextFire.Sub(clock.Now())):
+				t.executer.execute()
+				progress.CompletedExecutions++
+				progress.LastExecution = nextFire
+				updateProgress(progress)
+
+			case <-stopCh:
+				return
+			}
+		}
+
+	}()
+
+	return cancel
+}
