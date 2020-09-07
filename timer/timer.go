3b557c19bdfa1fde99974ee3bcc5ae125d2604f6
nivista <syaniv@live.com>
2020-09-07 14:10:37 -0700

    implement timer

--- timer/timer.go
+++ timer/timer.go
@@ -1,11 +1,15 @@
 package timer
 
 import (
+	"errors"
+	"sync"
 	"time"
 
+	"github.com/golang/protobuf/ptypes/timestamp"
 	"github.com/jonboulle/clockwork"
 
 	"github.com/nivista/steady/internal/.gen/protos/messaging"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type (
@@ -21,9 +25,129 @@ type (
 		Stop()
 	}
 
-	// defining type here rather than using only protobuf Progress type, this can be safely copied
+	timer struct {
+		pk string
+
+		execute
+		schedule
+		progress
+
+		active bool
+		ch     chan struct{}
+		mux    sync.RWMutex
+	}
+
+	// zrather than using protobuf, this can be safely copied.
 	progress struct {
 		completedExecutions int32
 		lastExecution       time.Time
 	}
 )
+
+// IsValid validates a create timer message.
+func IsValid(p *messaging.Create) error {
+	if _, err := validateSchedule(p.Schedule); err != nil {
+		return errors.New("invalid schedule: " + err.Error())
+	}
+
+	if err := validateTask(p.Task); err != nil {
+		return errors.New("invalid task: " + err.Error())
+	}
+
+	return nil
+}
+
+// New creates a Timer from a create timer message and a primary key.
+func New(p *messaging.Create, pk string) (Timer, error) {
+	if err := IsValid(p); err != nil {
+		return nil, errors.New("new timer: " + err.Error())
+	}
+
+	return &timer{
+		pk:       pk,
+		execute:  newExecute(p.Task),
+		schedule: newSchedule(p.Schedule),
+		ch:       make(chan struct{}),
+	}, nil
+}
+
+func (t *timer) WithProgress(pb *messaging.Progress) Timer {
+	return &timer{
+		pk:       t.pk,
+		execute:  t.execute,
+		schedule: t.schedule,
+		progress: progressFromProto(pb),
+		ch:       make(chan struct{}),
+	}
+}
+
+func (t *timer) Start(executeTimer func(execMsg *messaging.Execute, pk string), finishTimer func(pk string), clock clockwork.Clock) {
+	t.mux.Lock()
+	defer t.mux.Unlock()
+
+	if t.active {
+		return
+	}
+	t.active = true
+
+	go func() {
+
+		for {
+			var nextFire = t.schedule(t.progress, clock.Now())
+
+			if nextFire == nil {
+				finishTimer(t.pk)
+				return
+			}
+
+			select {
+			case now := <-clock.After(nextFire.Sub(clock.Now())):
+				res := t.execute()
+
+				t.progress.completedExecutions++
+				t.progress.lastExecution = now
+
+				executeTimer(&messaging.Execute{
+					Progress: progressToProto(t.progress),
+					Result:   res,
+				}, t.pk)
+
+			case <-t.ch:
+				return
+			}
+		}
+	}()
+
+}
+
+func (t *timer) Stop() {
+	t.mux.Lock()
+	defer t.mux.Unlock()
+
+	if t.active {
+		t.ch <- struct{}{} // block until timer stops
+		t.active = false
+	}
+}
+
+func progressToProto(p progress) *messaging.Progress {
+	var last *timestamp.Timestamp
+	if !p.lastExecution.IsZero() {
+		last = timestamppb.New(p.lastExecution)
+	}
+	return &messaging.Progress{
+		CompletedExecutions: p.completedExecutions,
+		LastExecution:       last,
+	}
+}
+
+func progressFromProto(pb *messaging.Progress) progress {
+	var last time.Time
+	if pb.LastExecution != nil {
+		last = pb.LastExecution.AsTime()
+	}
+	return progress{
+		completedExecutions: pb.CompletedExecutions,
+		lastExecution:       last,
+	}
+}
