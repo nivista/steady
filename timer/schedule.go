05e146389746ad1e707e6d2db3060b9cef5fbbc7
nivista <syaniv@live.com>
2020-09-22 12:27:49 -0700

    require schedule start time not nil

--- timer/schedule.go
+++ timer/schedule.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/nivista/steady/.gen/protos/common"
 	"github.com/robfig/cron"
-	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type schedule func(prog progress, now time.Time) *time.Time
@@ -16,9 +15,13 @@ var parser = cron.NewParser(cron.Minute | cron.Hour | cron.Dom | cron.Month | cr
 const rollback = -1 * time.Nanosecond
 
 func newSchedule(p *common.Schedule) (schedule, error) {
-	var sched, err = parser.Parse(p.Cron)
+	sched, err := parser.Parse(p.Cron)
 	if err != nil {
 		return nil, fmt.Errorf("invalid cron: %w", err)
+	}
+
+	if p.StartTime == nil {
+		return nil, fmt.Errorf("start time required")
 	}
 
 	return func(prog progress, now time.Time) *time.Time {
@@ -29,16 +32,13 @@ func newSchedule(p *common.Schedule) (schedule, error) {
 
 		var beforeNextFire time.Time
 		if prog.lastExecution == nil { // this is the first execution
-			if p.StartTime == nil { // start time wasn't specified, start now
-				p.StartTime = timestamppb.New(now)
-			}
 			beforeNextFire = p.StartTime.AsTime().Add(rollback)
 		} else {
 			beforeNextFire = *prog.lastExecution
 		}
 
 		// find next fire
-		var nextFire = sched.Next(beforeNextFire)
+		nextFire := sched.Next(beforeNextFire)
 
 		if nextFire.Before(now) { // missed fire
 			nextFire = now // compensate for missed fire
