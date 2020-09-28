f2db742eb4f9060a81551ca911214365c1a855a2
nivista <syaniv@live.com>
2020-09-27 17:10:40 -0700

    implement basic auth helpers

--- frontend/util/util.go
+++ frontend/util/util.go
@@ -1,9 +1,30 @@
 package util
 
 import (
+	"encoding/base64"
 	"path"
 	"strings"
 )
+
+// ParseBasicAuth parses an HTTP Basic Authentication string.
+// copied from "net/http"
+func ParseBasicAuth(auth string) (username, password string, ok bool) {
+	const prefix = "Basic "
+	// Case insensitive prefix match. See Issue 22736.
+	if len(auth) < len(prefix) || !strings.EqualFold(auth[:len(prefix)], prefix) {
+		return
+	}
+	c, err := base64.StdEncoding.DecodeString(auth[len(prefix):])
+	if err != nil {
+		return
+	}
+	cs := string(c)
+	s := strings.IndexByte(cs, ':')
+	if s < 0 {
+		return
+	}
+	return cs[:s], cs[s+1:], true
+}
 
 // ShiftPath returns the topmost directory of the path, and the rest of the path.
 func ShiftPath(p string) (head, tail string) {
